d6be70efdad5b0efa865a0b46ebf8c79bf49e630
RizkiMufrizal <mufrizalrizki@gmail.com>
2017-03-08 22:59:04 +0700

    add constant variabel

---
+++ app/src/main/java/com/rizkimufrizal/belajar/retrofit/configuration/Constant.java
@@ -0,0 +1,9 @@
+package com.rizkimufrizal.belajar.retrofit.configuration;
+
+/**
+ * Created by rizkimufrizal on 3/8/17.
+ */
+
+public class Constant {
+    public static final String BASE_API_URL = "https://api.spotify.com/v1/";
+}
